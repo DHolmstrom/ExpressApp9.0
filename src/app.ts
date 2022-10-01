b9f8756763320ca2832ad6c662dfd58b08da4d08
DHolmstrom <dennis@holmstrom.nu>
2022-10-01 22:12:34 +0200

    Setup ejs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from 'config';
 import session from 'express-session';
+import expressLayouts from 'express-ejs-layouts';
 
 import connect from './utils/connect';
 import logger from './utils/logger';
@@ -19,6 +20,13 @@ const port = config.get<number>('port');
 const app = express();
 
 app.use(express.json());
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use('/', express.static(__dirname + '/../public'));
+app.set('views', __dirname + '/views');
 
 const oneDay = 1000 * 60 * 60 * 24;
 const sessionSecret = config.get<string>('sessionSecret');
--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,20 @@ import albumAPIRouter from './routes/album.api';
 import userAPIRouter from './routes/user.api';
 
 const routes = (app: Express) => {
-  app.get('/test', (req: Request, res: Response) => {
+  app.get('/api/test', (req: Request, res: Response) => {
     res.status(200).send('App works fine');
+  });
+
+  app.get('/', (req: Request, res: Response) => {
+    res.render('pages/index');
+  });
+
+  app.get('/albums', (req: Request, res: Response) => {
+    res.render('pages/albums');
+  });
+
+  app.get('/albums', (req: Request, res: Response) => {
+    res.render('pages/images');
   });
 
   app.use('/api/albums', albumAPIRouter);
