017120f3f07de99bc14031fb7c623db597f1f66c
DHolmstrom <dennis@holmstrom.nu>
2022-10-01 22:12:56 +0200

    imported old files from prev app

---
+++ public/css/js/main.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const mainNavToggle = document.querySelector('.toggle__main__nav');
+if (mainNavToggle) {
+  mainNavToggle.addEventListener('click', () => {
+    body.classList.toggle('open_nav');
+    header.classList.toggle('open_nav');
+  });
+}
