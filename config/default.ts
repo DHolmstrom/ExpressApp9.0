4352cbf944765f2a9dcf7b587c7d4fc2ed1dada9
DHolmstrom <dennis@holmstrom.nu>
2022-10-01 13:22:55 +0200

    Setup express session

--- config/default.ts
+++ config/default.ts
@@ -3,4 +3,5 @@ export default {
   dbUri:
     'mongodb+srv://holmstromsfoto:QizqqEPIW8uKKlgE@holmstromsfoto.muni5.mongodb.net/HolmstromsFoto',
   saltRounds: 10,
+  sessionSecret: 'bF1sG3Qzjpd0syrEWFaeY4gsQobSkHakBjMXm3Hw',
 };
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,35 @@
 import express from 'express';
 import config from 'config';
+import session from 'express-session';
 
 import connect from './utils/connect';
 import logger from './utils/logger';
 import routes from './routes';
+
+import { UserDocument } from './models/user.model';
+
+declare module 'express-session' {
+  interface SessionData {
+    user: any | undefined;
+  }
+}
 
 const port = config.get<number>('port');
 
 const app = express();
 
 app.use(express.json());
+
+const oneDay = 1000 * 60 * 60 * 24;
+const sessionSecret = config.get<string>('sessionSecret');
+app.use(
+  session({
+    secret: sessionSecret,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
 
 app.listen(port, async () => {
   logger.info(`App is runging att http://localhost:${port}`);
