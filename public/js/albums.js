a175c110432549565d6c8981c34571dfb7fec419
DHolmstrom <dennis@holmstrom.nu>
2022-10-04 10:54:04 +0200

    Refactored JS for album page

--- public/js/albums.js
+++ public/js/albums.js
@@ -3,28 +3,33 @@ const albumsGrid = document.querySelector('.albums__grid');
 fetch('/api/albums')
   .then((res) => res.json())
   .then((res) => {
-    console.log(res);
-    res.forEach((album) => {
-      const {
-        _id,
-        title = '',
-        subTitle = '',
-        images: { filename, tags = [] },
-      } = album;
-      var albumCoverImg = document.createElement('img');
-      albumCoverImg.src = `/images/lowres/${filename}`;
-      albumCoverImg.alt = `${tags.join(', ')}`;
-      var albumCoverTitle = document.createElement('h6');
-      albumCoverTitle.classList.add('album__title');
-      albumCoverTitle.appendChild(document.createTextNode(title));
-      var albumCoverInfo = document.createElement('p');
-      albumCoverInfo.appendChild(document.createTextNode(subTitle));
-      var albumCoverA = document.createElement('a');
-      albumCoverA.href = `/albums/${_id}`;
-      albumCoverA.classList.add('album');
-      albumCoverA.appendChild(albumCoverImg);
-      albumCoverA.appendChild(albumCoverTitle);
-      albumCoverA.appendChild(albumCoverInfo);
-      albumsGrid.appendChild(albumCoverA);
-    });
+    res.forEach(createAlbumItem);
   });
+
+function createAlbumItem(album) {
+  const {
+    _id,
+    title = '',
+    subTitle = '',
+    images: { filename, tags = [] },
+  } = album;
+  const albumCoverImg = document.createElement('img');
+  albumCoverImg.src = `/images/lowres/${filename}`;
+  albumCoverImg.alt = `${tags.join(', ')}`;
+
+  const albumCoverTitle = document.createElement('h6');
+  albumCoverTitle.classList.add('album__title');
+  albumCoverTitle.appendChild(document.createTextNode(title));
+
+  const albumCoverInfo = document.createElement('p');
+  albumCoverInfo.appendChild(document.createTextNode(subTitle));
+
+  const albumCoverA = document.createElement('a');
+  albumCoverA.href = `/albums/${_id}`;
+  albumCoverA.classList.add('album');
+
+  albumCoverA.appendChild(albumCoverImg);
+  albumCoverA.appendChild(albumCoverTitle);
+  albumCoverA.appendChild(albumCoverInfo);
+  albumsGrid.appendChild(albumCoverA);
+}
